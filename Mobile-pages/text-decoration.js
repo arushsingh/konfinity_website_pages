fa641a78648b867d5d9fa1492bdde269b10dbb85
arushsingh <arushsingh1234@gmail.com>
2018-04-01 19:19:45 +0530

    upload

--- Mobile-pages/text-decoration.js
+++ Mobile-pages/text-decoration.js
@@ -1,11 +1,14 @@
 function headOn1(){
     document.getElementById("header").style.textDecoration="underline";
+    document.getElementById("header").style.webkitTextDecorationColor="#4e86d8";
 }
 
 function headOn2(){
     document.getElementById("header1").style.textDecoration="underline";
+    document.getElementById("header1").style.webkitTextDecorationColor="#FFDB58";
 }
 
 function headOn3(){
     document.getElementById("header2").style.textDecoration="underline";
+    document.getElementById("header2").style.webkitTextDecorationColor="red";
 }
