c4ac14816931edbe2082a6d8b02a670f14d15f6c
arushsingh <arushsingh1234@gmail.com>
2018-04-01 19:03:54 +0530

    update

---
+++ Mobile-pages/text-decoration.js
@@ -0,0 +1,11 @@
+function headOn1(){
+    document.getElementById("header").style.textDecoration="underline";
+}
+
+function headOn2(){
+    document.getElementById("header1").style.textDecoration="underline";
+}
+
+function headOn3(){
+    document.getElementById("header2").style.textDecoration="underline";
+}
